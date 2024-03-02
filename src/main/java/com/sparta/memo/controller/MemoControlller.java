c5d86c9db3542836426e5309b935c3f33973fe1c
dayounglee <verdantjuly@kakao.com>
2024-03-02 10:28:00 +0900

    feat : DELETE UPDATE API

--- src/main/java/com/sparta/memo/controller/MemoControlller.java
+++ src/main/java/com/sparta/memo/controller/MemoControlller.java
@@ -38,4 +38,28 @@ public class MemoControlller {
                 .map(MemoResponseDto::new).toList();
         return responseList;
     }
+
+    @PutMapping("/memos/{id}")
+    public Long upadateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
+            memo.update(requestDto);
+            return memo.getId();
+        } else{
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public  Long deleteMemo(@PathVariable Long id){
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            memoList.remove(id);
+            return id;
+        } else{
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
 }
