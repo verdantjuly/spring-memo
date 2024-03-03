46b12be21b7333119354ea6fe067943037c5a3b4
dayounglee <verdantjuly@kakao.com>
2024-03-04 08:39:10 +0900

    feat : Query Methods

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,7 +3,11 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo,  Long> {
+
+    List<Memo> findAllByOrderByModifiedAtDesc();
 
 
 }
