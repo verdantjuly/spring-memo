6d268d85b4fe712b5127234ffffb4973acb3a2d6
dayounglee <verdantjuly@kakao.com>
2024-03-03 22:00:07 +0900

    feat : DI/IoC

---
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -0,0 +1,76 @@
+package com.sparta.memo.service;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import com.sparta.memo.repository.MemoRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MemoService {
+
+
+    private  final MemoRepository memoRepository;
+
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
+    }
+
+
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
+
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // DB 저장
+
+        Memo saveMemo = memoRepository.save(memo);
+
+        // Entity -> ResponseDto
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+        return memoResponseDto;
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        // DB 조회
+
+        return memoRepository.findAll();
+
+
+    }
+
+    public Long updateMemo(Long id, MemoRequestDto requestDto) {
+        // DB 수정
+
+
+
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);
+        if (memo != null) {
+            // memo 내용 수정
+            memoRepository.update(id, requestDto);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+
+
+    public Long deleteMemo(Long id) {
+
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);
+        if (memo != null) {
+            // memo 삭제
+           memoRepository.delete( id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+}
